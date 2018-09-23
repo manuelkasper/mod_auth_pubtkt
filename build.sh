67f8943b453b3c8771c043a031b0fe91bc15f371
rmacd <ronald@rmacd.com>
2018-09-23 04:07:02 +0100

    add build helper

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+set +x
+
+MOD=mod_auth_pubtkt
+VERSION=0.12
+
+# nuke rubbish we don't want
+find . -type f -name Makefile -delete
+rm -f configure
+
+autoreconf -vfi
+./configure
+make
+
+if [[ $1 =~ "rpm" ]] ; then
+    if [[ -e "$MOD.spec" ]] ; then
+        mydir=`dirname $0`
+        tmpdir=`mktemp -d`
+        
+        # sanity check
+        if [ -z "$tmpdir" ] ; then
+            echo "Error creating tmpdir"
+            exit 1
+        fi
+
+        set +x
+
+        cp -r "${mydir}" "${tmpdir}/${MOD}-${VERSION}"
+        tar -czf "${tmpdir}/${MOD}-${VERSION}.tar.gz" --exclude=".git" -C "${tmpdir}" "${MOD}-${VERSION}"
+        rpmbuild -ta "${tmpdir}/${MOD}-${VERSION}.tar.gz"
+        rm -rf "${tmpdir}"
+    else
+        echo "Missing RPM spec file"
+        exit 1
+    fi
+fi
